933ebd3d03790adfd70f894292b172cf7e6134d5
Jędrzej Jakubowski <jedrzej.jakubowski1@gmail.com>
2022-07-23 13:15:19 +0200

    change env vars to placeholders in dev_env.sh

--- dev_env.sh
+++ dev_env.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-export SECRET_KEY="ni^!n2d#!)(3bgt)_3#w)fwa-f_!w!_ularz=nov_*lu!%t4l3"
-export DEBUG=1
-export DJANGO_SUPERUSER_EMAIL=admin@admin.com
-export DJANGO_SUPERUSER_USERNAME=django_user
-export DJANGO_SUPERUSER_PASSWORD=django_pass
-export POSTGRES_USER=gis_user
-export POSTGRES_PASSWORD=gis_pass
-export POSTGRES_DB=gis_db
+export SECRET_KEY=# secret key
+export DEBUG=# 0 or 1
+export DJANGO_SUPERUSER_EMAIL=# admin email
+export DJANGO_SUPERUSER_USERNAME=# admin username
+export DJANGO_SUPERUSER_PASSWORD=# admin password
+export POSTGRES_USER=# postgis username
+export POSTGRES_PASSWORD=# postgis password
+export POSTGRES_DB=# postgis database name
 
 source venv/bin/activate
