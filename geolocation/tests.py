366512df6dd8bb7cf686c9c50faf6a8ec5851efe
Jędrzej Jakubowski <jedrzej.jakubowski1@gmail.com>
2022-08-10 19:13:00 +0200

    add location serializer field content tests

--- geolocation/tests.py
+++ geolocation/tests.py
@@ -177,6 +177,12 @@ class LocationTests(APITestCase):
         self.language_1 = Language.objects.create(**{'code':'AA','name':'AAA','native':'AAA'})
         self.language_2 = Language.objects.create(**{'code':'BB','name':'BBB','native':'BBB'})
 
+    def test_serializer_expected_fields(self):
+        payload_data = {'geoname_id':12345,'capital':'Capital City','languages':[self.language_1.pk, self.language_2.pk]}
+        serializer = LocationSerializer(data=payload_data)
+        serializer.is_valid(raise_exception=True)
+        self.assertEqual(serializer.validated_data.keys(), payload_data.keys())
+
     def test_geoname_id_none(self):
         serializer = LocationSerializer(data={'geoname_id':None,'capital':'Capital City','languages':[self.language_1.pk, self.language_2.pk]})
         self.assertTrue(serializer.is_valid(raise_exception=True))
@@ -197,6 +203,11 @@ class LocationTests(APITestCase):
             serializer.is_valid(raise_exception=True)
     
         self.assertEqual(cm.exception.detail['geoname_id'][0].code, 'min_value')
+
+    def test_geoname_field_content(self):
+        serializer = LocationSerializer(data={'geoname_id':12345,'capital':'Capital City','languages':[self.language_1.pk, self.language_2.pk]})
+        serializer.is_valid(raise_exception=True)
+        self.assertEqual(serializer.validated_data['geoname_id'], 12345)
     
     def test_capital_blank_positive(self):
         serializer = LocationSerializer(data={'geoname_id':12345,'capital':'','languages':[self.language_1.pk, self.language_2.pk]})
@@ -226,6 +237,11 @@ class LocationTests(APITestCase):
         location = serializer.save()
         self.assertIsInstance(location, Location)
 
+    def test_capital_field_content(self):
+        serializer = LocationSerializer(data={'geoname_id':12345,'capital':'Capital City','languages':[self.language_1.pk, self.language_2.pk]})
+        serializer.is_valid(raise_exception=True)
+        self.assertEqual(serializer.validated_data['capital'], 'Capital City')
+
     
     def test_languages_null_negative(self):
         with self.assertRaisesMessage(ValidationError, 'This field may not be null.') as cm:
@@ -234,12 +250,29 @@ class LocationTests(APITestCase):
         
         self.assertEqual(cm.exception.detail['languages'][0].code, 'null')
     
-    def test_languages_positive(self):
+    def test_languages_field_content_pk_write_positive(self):
         serializer = LocationSerializer(data={'geoname_id':12345,'capital':'Capital City','languages':[self.language_1.pk, self.language_2.pk]})
         self.assertTrue(serializer.is_valid(raise_exception=True))
         
         location = serializer.save()
         self.assertIsInstance(location, Location)
+    
+    def test_languages_field_content_instance_write_negative(self):
+        with self.assertRaisesMessage(ValidationError, 'Incorrect type. Expected pk value, received Language.') as cm:
+            serializer = LocationSerializer(data={'geoname_id':12345,'capital':'Capital City','languages':[self.language_1, self.language_2]})
+            self.assertTrue(serializer.is_valid(raise_exception=True))
+        
+        self.assertEqual(cm.exception.detail['languages'][0].code, 'incorrect_type')
+
+    def test_languages_to_representation_instance_read_positive(self):
+        serializer = LocationSerializer(data={'geoname_id':12345,'capital':'Capital City','languages':[self.language_1.pk, self.language_2.pk]})
+        serializer.is_valid(raise_exception=True)
+        self.assertEqual(serializer.validated_data['languages'], [self.language_1, self.language_2])
+
+    def test_languages_to_representation_pk_read_negative(self):
+        serializer = LocationSerializer(data={'geoname_id':12345,'capital':'Capital City','languages':[self.language_1.pk, self.language_2.pk]})
+        serializer.is_valid(raise_exception=True)
+        self.assertNotEqual(serializer.validated_data['languages'], [self.language_1.pk, self.language_2.pk])
         
 
     def tearDown(self) -> None:
