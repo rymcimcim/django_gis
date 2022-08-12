f4c47b296c0bc82dbc8461165170d91d8e50212d
Jędrzej Jakubowski <jedrzej.jakubowski1@gmail.com>
2022-08-12 20:36:35 +0200

    add LanguageViewSetTests and pagination

--- django_gis/settings.py
+++ django_gis/settings.py
@@ -145,7 +145,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAdminUser',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 SIMPLE_JWT = {
--- languages/tests.py
+++ languages/tests.py
@@ -1,9 +1,18 @@
-from django.test import tag
+import json
 
+from django.contrib.auth.models import User
+from django.http.response import Http404
+from django.shortcuts import get_object_or_404
+from django.test import tag
+from django.urls import reverse
+
+from rest_framework import status
 from rest_framework.exceptions import ValidationError
-from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory, APITestCase
 
 from languages.serializers import LanguageSerializer
+from languages.models import Language
+from languages.views import LanguageViewSet
 
 
 @tag('language', 'serializer')
@@ -80,8 +89,88 @@ class LanguageSerializerTests(APITestCase):
             serializer = LanguageSerializer(data={'code':'AA','name':'AAA','native':'AAA'})
             serializer.is_valid(raise_exception=True)
 
+        self.assertEqual(Language.objects.count(), 1)
         self.assertEqual(cm.exception.detail['non_field_errors'][0].code, 'unique')
     
     def test_language_positive(self):
         serializer = LanguageSerializer(data={'code':'AA','name':'AAA','native':'AAA'})
         serializer.is_valid(raise_exception=True)
+
+
+@tag('language', 'viewset')
+class LanguageViewSetTests(APITestCase):
+    def setUp(self) -> None:
+        self.language_1 = Language.objects.create(**{'code':'AA','name':'AAA','native':'AAA'})
+        self.language_2 = Language.objects.create(**{'code':'BB','name':'BBB','native':'BBB'})
+        self.rf_client = APIRequestFactory(enforce_csrf_checks=True)
+        User.objects.create_superuser(username='test_user', email='test_user@test.com', password='test_pass')
+        response = self.client.post(
+            reverse('token_obtain_pair'),
+            json.dumps({"username": "test_user", "password": "test_pass"}),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.token = response.data["access"]
+
+    def test_superuser_can_see_languages(self):
+        view = LanguageViewSet.as_view({'get': 'list'})
+        request = self.rf_client.get(reverse('api:languages-list'), HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('count'), 2)
+
+        queryset = Language.objects.filter(id__in=[self.language_1.pk, self.language_2.pk])
+        data = LanguageSerializer(queryset, many=True).data
+        self.assertListEqual(response.data.get("results"), data)
+    
+    def test_superuser_can_see_single_language(self):
+        view = LanguageViewSet.as_view({'get': 'retrieve'})
+        request = self.rf_client.get(reverse('api:languages-detail', args=(self.language_1.pk,)), HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = view(request, pk=self.language_1.pk)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = LanguageSerializer(self.language_1).data
+        self.assertDictEqual(response.data, data)
+    
+    def test_superuser_can_see_delete_language(self):
+        view = LanguageViewSet.as_view({'delete': 'destroy'})
+        pk = self.language_1.pk
+        request = self.rf_client.delete(reverse('api:languages-detail', args=(pk,)), HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = view(request, pk=pk)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        with self.assertRaisesMessage(Http404, 'No Language matches the given query.'):
+            get_object_or_404(Language, pk=pk)
+    
+    def test_superuser_can_see_single_language(self):
+        view = LanguageViewSet.as_view({'get': 'retrieve'})
+        request = self.rf_client.get(reverse('api:languages-detail', args=(self.language_1.pk,)), HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = view(request, pk=self.language_1.pk)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = LanguageSerializer(self.language_1).data
+        self.assertDictEqual(response.data, data)
+    
+    def test_superuser_can_update_language(self):
+        view = LanguageViewSet.as_view({'put': 'update'})
+        update_payload = {'code':'CC','name':'CCC','native':'CCC'}
+        pk = self.language_1.pk
+        request = self.rf_client.put(reverse('api:languages-detail', args=(pk,)), update_payload, HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = view(request, pk=pk)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        language = get_object_or_404(Language, pk=pk)
+        data = LanguageSerializer(language).data
+        self.assertDictEqual(response.data, data)
+    
+    def test_superuser_can_partial_update_language(self):
+        view = LanguageViewSet.as_view({'patch': 'partial_update'})
+        update_payload = {'code':'CC'}
+        pk = self.language_1.pk
+        request = self.rf_client.patch(reverse('api:languages-detail', args=(pk,)), update_payload, HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response = view(request, pk=pk)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        language = get_object_or_404(Language, pk=pk)
+        data = LanguageSerializer(language).data
+        self.assertDictEqual(response.data, data)
